f43a5f7fa6e3d5506b77bf69f4cd31643254d349
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-09-06 00:14:35 +0700

    80

--- Text-mode_Game/Source.cpp
+++ Text-mode_Game/Source.cpp
@@ -4,20 +4,21 @@
 
 void draw_ship(int, int);
 void gotoxy(int, int);
+void erase_ship(int, int);
 
 int main()
 
 {
 	char ch = ' ';
-	int x = 38, y = 20;
+	int x = 0, y = 0;
 	draw_ship(x, y);
 	do {
 		if (_kbhit()) {
 			ch = _getch();
-			if (ch == 'a') { draw_ship(--x, y); }
-			if (ch == 'd') { draw_ship(++x, y); }
-			if (ch == 'w') { draw_ship(x, ++y); }
-			if (ch == 's') { draw_ship(x, --y); }
+			if (ch == 'a' && x > 0) { erase_ship(x, y); draw_ship(--x, y); }
+			if (ch == 'd' && x < 80) { erase_ship(x, y); draw_ship(++x, y); }
+			if (ch == 'w' && y > 0) { erase_ship(x, y); draw_ship(x, --y); }
+			if (ch == 's' && y < 25) { erase_ship(x, y); draw_ship(x, ++y); }
 			fflush(stdin);
 		}
 		Sleep(100);
@@ -29,6 +30,12 @@ void draw_ship(int x,int y)
 {
 	gotoxy(x, y);
 	printf(" <-0-> ");
+}
+
+void erase_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf("       ");
 }
 
 void gotoxy(int x, int y)
