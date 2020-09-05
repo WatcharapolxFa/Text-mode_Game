5acdd4b9e2f7f49e835d278db5d61b7a546ec4ef
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-09-06 00:02:51 +0700

    10

--- Text-mode_Game/Source.cpp
+++ Text-mode_Game/Source.cpp
@@ -1,19 +1,38 @@
 #include<stdio.h>
+#include<conio.h>
 #include<windows.h>
+
+void draw_ship(int, int);
+void gotoxy(int, int);
+
+int main()
+
+{
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a') { draw_ship(--x, y); }
+			if (ch == 'd') { draw_ship(++x, y); }
+			if (ch == 'w') { draw_ship(x, ++y); }
+			if (ch == 's') { draw_ship(x, --y); }
+			fflush(stdin);
+		}
+		Sleep(100);
+	} while (ch != 'x');
+	return 0;
+}
+
+void draw_ship(int x,int y)
+{
+	gotoxy(x, y);
+	printf(" <-0-> ");
+}
+
 void gotoxy(int x, int y)
 {
 	COORD c = { x, y };
-	SetConsoleCursorPosition(
-		GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-void daw_ship()
-{
-  printf("<.0.>");
-}
-int main() 
-{
-	gotoxy(10,10);
-	daw_ship();
-	return 0;
-
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
