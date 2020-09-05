157d32d09e538e742139ed477e31f814483a2c88
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-09-06 00:24:40 +0700

    SAD

--- Text-mode_Game/Source.cpp
+++ Text-mode_Game/Source.cpp
@@ -10,15 +10,15 @@ int main()
 
 {
 	char ch = ' ';
-	int x = 0, y = 0;
+	int x = 113, y = 30;
 	draw_ship(x, y);
 	do {
 		if (_kbhit()) {
 			ch = _getch();
 			if (ch == 'a' && x > 0) { erase_ship(x, y); draw_ship(--x, y); }
-			if (ch == 'd' && x < 80) { erase_ship(x, y); draw_ship(++x, y); }
+			if (ch == 'd' && x < 113) { erase_ship(x, y); draw_ship(++x, y); }
 			if (ch == 'w' && y > 0) { erase_ship(x, y); draw_ship(x, --y); }
-			if (ch == 's' && y < 25) { erase_ship(x, y); draw_ship(x, ++y); }
+			if (ch == 's' && y < 30) {erase_ship(x, y); draw_ship(x, ++y); }
 			fflush(stdin);
 		}
 		Sleep(100);
@@ -28,7 +28,7 @@ int main()
 
 void draw_ship(int x,int y)
 {
-	gotoxy(x, y);
+	gotoxy(x,y);
 	printf(" <-0-> ");
 }
 
